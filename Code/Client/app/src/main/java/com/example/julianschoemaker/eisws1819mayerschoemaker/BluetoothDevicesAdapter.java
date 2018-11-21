fc2688270bbcff3f1315d60e2b1db008063163bb
JulianSchoem <julian.schoemaker@smail.th-koeln.de>
2018-11-21 20:50:08 +0100

    update on files for Johanna

--- Code/Client/app/src/main/java/com/example/julianschoemaker/eisws1819mayerschoemaker/BluetoothDevicesAdapter.java
+++ Code/Client/app/src/main/java/com/example/julianschoemaker/eisws1819mayerschoemaker/BluetoothDevicesAdapter.java
@@ -2,19 +2,24 @@ package com.example.julianschoemaker.eisws1819mayerschoemaker;
 
 import android.bluetooth.BluetoothDevice;
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class BluetoothDevicesAdapter extends ArrayAdapter<BluetoothDevice> {
 
     private LayoutInflater layoutInflater;
     private ArrayList<BluetoothDevice> devices;
     private int viewResourceID;
+
 
     public BluetoothDevicesAdapter(Context context, int resourceID, ArrayList<BluetoothDevice> devices) {
         super(context, resourceID, devices);
@@ -23,14 +28,18 @@ public class BluetoothDevicesAdapter extends ArrayAdapter<BluetoothDevice> {
         viewResourceID = resourceID;
     }
 
+
+
     public View getView(int position, View convertView, ViewGroup parent) {
         convertView = layoutInflater.inflate(viewResourceID, null);
 
-        BluetoothDevice device = devices.get(position);
+
+
+        final BluetoothDevice device = devices.get(position);
 
         if (device != null) {
-            TextView deviceName = convertView.findViewById(R.id.deviceName);
-            TextView deviceAddress = convertView.findViewById(R.id.deviceAddress);
+            final TextView deviceName = convertView.findViewById(R.id.deviceName);
+            final TextView deviceAddress = convertView.findViewById(R.id.deviceAddress);
 
             if (deviceName != null) {
                 deviceName.setText(device.getName());
@@ -39,9 +48,13 @@ public class BluetoothDevicesAdapter extends ArrayAdapter<BluetoothDevice> {
             if (deviceAddress != null) {
                 deviceAddress.setText(device.getAddress());
             }
+
+
         }
 
         return convertView;
     }
+
+
 
 }
--- Code/Client/app/src/main/java/com/example/julianschoemaker/eisws1819mayerschoemaker/contact.java
+++ Code/Client/app/src/main/java/com/example/julianschoemaker/eisws1819mayerschoemaker/contact.java
@@ -1,15 +1,32 @@
 package com.example.julianschoemaker.eisws1819mayerschoemaker;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.UUID;
 
 public class contact extends AppCompatActivity {
 
     public static String BlueID = "BTID";
 
+    BluetoothConnectionService mBluetoothConnection;
+
+    private static final UUID MY_UUID_INSECURE =
+            UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+
     TextView textview_bluetoothID;
+    Button btnConnect;
+
+    BluetoothDevice device;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +38,18 @@ public class contact extends AppCompatActivity {
         BlueID = getIntent().getExtras().getString(BlueID);
 
         textview_bluetoothID.setText(BlueID);
+
+        btnConnect = findViewById(R.id.buttonConnect);
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Bluetooth Device herausfinden! Toast funktioniert generell, Methode startClient ohne device nicht zu testen...
+                Toast toast = Toast.makeText(getApplicationContext(), "Waiting for: " + device.getName() + " with ID: " + device.getAddress(), Toast.LENGTH_LONG);
+                toast.show();
+
+                //mBluetoothConnection.startClient(device, MY_UUID_INSECURE);
+            }
+        });
     }
+
 }
