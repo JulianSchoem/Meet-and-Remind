426f0f380c882d6a1eeba7bf3cf0c57d2a76e94d
Jojoblume <johanna.mayer@smail.th-koeln.de>
2019-01-17 19:31:52 +0100

    Clean Code, Add Comments

--- Client/app/src/main/java/com/example/julianschoemaker/eisws1819mayerschoemaker/BluetoothConnectionService.java
+++ Client/app/src/main/java/com/example/julianschoemaker/eisws1819mayerschoemaker/BluetoothConnectionService.java
@@ -7,7 +7,6 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
@@ -17,46 +16,42 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.os.Build;
-import android.os.Bundle;
+
 import android.os.Handler;
 import android.os.Message;
-import android.os.ResultReceiver;
 
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.UUID;
 
-import static com.example.julianschoemaker.eisws1819mayerschoemaker.NotifyService.STATE_CONNECTED;
-import static com.example.julianschoemaker.eisws1819mayerschoemaker.NotifyService.STATE_CONNECTING;
-import static com.example.julianschoemaker.eisws1819mayerschoemaker.NotifyService.STATE_CONNECTION_FAILED;
+import static com.example.julianschoemaker.eisws1819mayerschoemaker.BluetoothConnectionService.STATE_CONNECTED;
+import static com.example.julianschoemaker.eisws1819mayerschoemaker.BluetoothConnectionService.STATE_CONNECTING;
+import static com.example.julianschoemaker.eisws1819mayerschoemaker.BluetoothConnectionService.STATE_CONNECTION_FAILED;
 
-public class NotifyService extends IntentService {
+public class BluetoothConnectionService extends IntentService {
 
     public static final String APP_NAME = "Meet And Remind";
-
     public static final UUID MY_UUID_INSECURE =
             UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
 
-    public static final int STATE_NONE = 0;       // we're doing nothing
-    public static final int STATE_LISTENING = 1;     // now listening for incoming connections
+    public static final int STATE_NONE = 0; // we're doing nothing
+    public static final int STATE_LISTENING = 1; // now listening for incoming connections
     public static final int STATE_CONNECTING = 2; // now initiating an outgoing connection
     public static final int STATE_CONNECTED = 3; // now connected to a remote device
-    public static final int STATE_CONNECTION_FAILED = 4;
+    public static final int STATE_CONNECTION_FAILED = 4; // connection failed
 
-    static String state = "NONE";
+    private static String state = "NONE";
 
     private static final int NOTIFICATION_ID = 1;
     private static final String NOTIFICATION_CHANNEL_ID = "my_notification_channel";
 
-    public NotifyService(){
+    public BluetoothConnectionService(){
         super("Notify Service");
     }
-
 
     @Override
     public void onCreate() {
@@ -68,7 +63,7 @@ public class NotifyService extends IntentService {
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
 
-        // Service läuft weiter, auch wenn App geschlossen wird.
+        // Service runs even if App is closed
         return START_STICKY;
     }
 
@@ -76,29 +71,31 @@ public class NotifyService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         while(intent == null){
+            // here Service runs if App is closed
             checkConnection();
         }
-
         while(true) {
-            // TODO Wo Server Socket erstellen? In Intent Service wäre praktisch, wenns geht..
             checkConnection();
         }
     }
 
     public void checkConnection(){
-
-        // TODO Should be in Shared Preferences for Improved Code
+        // TODO pairedDevicesList should be in Shared Preferences for improved Code
+        // TODO pairedDeviceList should be replaced with ContactList from Activity ContactList
         ArrayList<BluetoothDevice> pairedDeviceList = new ArrayList<>();
         BluetoothAdapter mybluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         Set<BluetoothDevice> bt = mybluetoothAdapter.getBondedDevices();
-
         if (bt.size() > 0) {
-
             for (BluetoothDevice device : bt) {
                 pairedDeviceList.add(device);
             }
         }
 
+        /**
+         * Client Side Application Logic
+         * Runs through PairedDevicesList, starts Client, Waits (Thread sleeps),
+         * if Connection was successful, send Notification.
+         */
         for (int i= 0; i<pairedDeviceList.size(); i++)
         {
             try {
@@ -109,8 +106,8 @@ public class NotifyService extends IntentService {
                 Thread.sleep(15000);
                 Log.v("abc", "(intent not null) state = " + state);
                 if (state.equals("CONNECTED")){
+                    // TODO Check if there is a Reminder to THIS Contact
                     sendNotification();
-
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -121,7 +118,7 @@ public class NotifyService extends IntentService {
     public void sendNotification(){
         NotificationManager nm = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
         Intent resultIntent = new Intent(getApplicationContext(), ReminderDetail.class);
-        // TODO which Reminder Detail? putExtra()/Parameter..!
+        // TODO GET Reminder Details that belongs to this Contact
         resultIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         PendingIntent resultPendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
@@ -133,7 +130,6 @@ public class NotifyService extends IntentService {
             nChannel.setLightColor(Color.CYAN);
             nm.createNotificationChannel(nChannel);
         }
-
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), NOTIFICATION_CHANNEL_ID)
                 .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
                 .setSmallIcon(R.drawable.logo_meet_remind)
@@ -170,32 +166,31 @@ public class NotifyService extends IntentService {
 
 }
 
+/**
+ * Server for Bluetooth Connection
+ * Initialized in Main Activity (ContactList)
+ */
 class ServerClass extends Thread{
 
     private BluetoothServerSocket serverSocket;
     public BluetoothAdapter mybluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-
     public ServerClass() {
-
         try {
-            serverSocket = mybluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(NotifyService.APP_NAME, NotifyService.MY_UUID_INSECURE);
+            serverSocket = mybluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(BluetoothConnectionService.APP_NAME, BluetoothConnectionService.MY_UUID_INSECURE);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void run(){
-
         BluetoothSocket socket = null;
-
         while(socket == null){
 
             try {
-
                 Message message = Message.obtain();
                 message.what = STATE_CONNECTING;
-                NotifyService.mHandler.sendMessage(message);
+                BluetoothConnectionService.mHandler.sendMessage(message);
 
                 socket = serverSocket.accept();
 
@@ -203,14 +198,7 @@ class ServerClass extends Thread{
                 e.printStackTrace();
                 Message message = Message.obtain();
                 message.what = STATE_CONNECTION_FAILED;
-                NotifyService.mHandler.sendMessage(message);
-            }
-
-            if (socket != null){
-                //send receive
-
-                break;
-
+                BluetoothConnectionService.mHandler.sendMessage(message);
             }
         }
     }
@@ -224,33 +212,25 @@ class ClientClass extends Thread{
     public ClientClass(BluetoothDevice device) {
         this.device = device;
         try {
-            socket = device.createRfcommSocketToServiceRecord(NotifyService.MY_UUID_INSECURE);
+            socket = device.createRfcommSocketToServiceRecord(BluetoothConnectionService.MY_UUID_INSECURE);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void run() {
-
         try {
-
             socket.connect();
             Message message = Message.obtain();
             message.what = STATE_CONNECTED;
-            NotifyService.mHandler.sendMessage(message);
-            //TODO Configure Push Notification
-            //device.getName() getReminder...
-
+            BluetoothConnectionService.mHandler.sendMessage(message);
         }
         catch (IOException e) {
             e.printStackTrace();
             Message message = Message.obtain();
             message.what = STATE_CONNECTION_FAILED;
-            NotifyService.mHandler.sendMessage(message);
+            BluetoothConnectionService.mHandler.sendMessage(message);
 
         }
-
     }
-
-
 }
